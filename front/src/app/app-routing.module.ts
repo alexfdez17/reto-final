aea8552d7f7bdc3bd231c4637fbf6c4fc1eff0fb
ainacarin <nacarinobazaga@gmail.com>
2019-09-13 11:34:44 +0200

    Basic Offers-list

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './parts/page-not-found/page-not-found.component';
 import {WelcomeComponent} from './parts/welcome/welcome.component';
 import {OfferAddComponent } from './offers/offer-add/offer-add.component';
+import { OffersListComponent } from './offers/offers-list/offers-list.component';
 
 const appRoutes: Routes = [
   {path: 'welcome', component: WelcomeComponent},
+  {path: 'offers', component: OffersListComponent},
   {path: 'offers/add', component: OfferAddComponent},
   {path: '', component: WelcomeComponent},
   {path: '**', component: PageNotFoundComponent}
--- front/src/app/offers/offers.module.ts
+++ front/src/app/offers/offers.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OfferAddComponent } from './offer-add/offer-add.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { OffersListComponent } from './offers-list/offers-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  declarations: [OfferAddComponent]
+  declarations: [OfferAddComponent, OffersListComponent]
 })
 export class OffersModule { }
