64f3c5825936ba6edebb4239b8c8c4132e37ecf7
javigala98 <jgalabermejo@gmail.com>
2019-09-13 11:18:54 +0200

    RestController implements method GET.ExpireDate, method DELETE, method PUT

--- back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
+++ back/src/main/java/org/springframework/samples/petclinic/rest/OfferRestController.java
@@ -1,8 +1,19 @@
 package org.springframework.samples.petclinic.rest;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.model.Offer;
 import org.springframework.samples.petclinic.service.OfferServiceImpl;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +23,28 @@ public class OfferRestController {
 	@Autowired
 	OfferServiceImpl offerSeImpl;
 	
+	@RequestMapping(value = "/notexpired", method = RequestMethod.GET)
+	public ResponseEntity<List<Offer>> getOffersNotExpired() {
+		List<Offer> offers = this.offerSeImpl.findByExpireDateAfter(new Date());
+		if (offers.isEmpty()) {
+			return new ResponseEntity<List<Offer>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<List<Offer>>(offers, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Offer> deleteAOffer(@PathVariable(value = "id") int id) {
+		this.offerSeImpl.delete(id);
+		return new ResponseEntity<Offer>(HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Offer> updateOffer(@RequestBody @Valid Offer offer, @PathVariable(value = "id") int id) {
+		if (offer.getId() != id) {
+			return new ResponseEntity<Offer>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Offer>(offerSeImpl.save(offer), HttpStatus.OK);
+	}
 	
 
 }
