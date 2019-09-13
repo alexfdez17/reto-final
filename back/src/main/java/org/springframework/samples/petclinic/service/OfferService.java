2c48d84f7811285f180bc203e49498b163e1b294
javigala98 <jgalabermejo@gmail.com>
2019-09-13 10:05:20 +0200

    OfferServiceImpl // Created RestController (Empty)

--- back/src/main/java/org/springframework/samples/petclinic/service/OfferService.java
+++ back/src/main/java/org/springframework/samples/petclinic/service/OfferService.java
@@ -1,6 +1,14 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
+import org.springframework.samples.petclinic.model.Offer;
+
 public interface OfferService{
 
+	public List <Offer> findAll();
+	public Offer findOne(int id);
+	public void delete(Offer offer);
+	public Offer save(Offer offer);
 	
 }
--- back/src/main/java/org/springframework/samples/petclinic/service/OfferServiceImpl.java
+++ back/src/main/java/org/springframework/samples/petclinic/service/OfferServiceImpl.java
@@ -1,11 +1,34 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Offer;
 import org.springframework.samples.petclinic.repository.OfferRepository;
 
 public class OfferServiceImpl implements OfferService{
 
 	@Autowired
 	OfferRepository offerRepository;
+
+	@Override
+	public List<Offer> findAll() {
+		return offerRepository.findAll();
+	}
+
+	@Override
+	public Offer findOne(int id) {
+		return offerRepository.findOne(id);
+	}
+
+	@Override
+	public Offer save(Offer offer) {
+		return offerRepository.saveAndFlush(offer);
+	}
+
+	@Override
+	public void delete(Offer offer) {
+		offerRepository.delete(offer);
+	}
 
 }
